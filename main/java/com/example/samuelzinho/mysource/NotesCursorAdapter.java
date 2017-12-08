831c59b6b858cf3038e22c42fea690a25036e54e
Sam Moreira <mor13009@byui.edu>
2017-12-08 16:37:00 -0700

    I have added some more comments to the NotesCursorSadapter class and also looked over the MainActivity class and its documentation of the code.

--- main/java/com/example/samuelzinho/mysource/NotesCursorAdapter.java
+++ main/java/com/example/samuelzinho/mysource/NotesCursorAdapter.java
@@ -10,20 +10,39 @@ import android.widget.TextView;
 
 /**
  * Created by Samuelzinho on 25/11/2017.
+ *
+ * Even though there is already a cursor adapter class we decided to make an extra class
+ * to be able to tailor it to our project. This class extends the CursorsAdapter class as
+ * its identifier.
  */
-
 public class NotesCursorAdapter extends CursorAdapter {
 
+    //This will be our constructor for this class.
     public NotesCursorAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
 
+    /**
+     * This will be the niewView function and it will bne able to reconcile the way that we
+     * want the notes to show and display in our UI using a LayoutInflater.
+     * @param context passes in the context
+     * @param cursor passes in the cursor object
+     * @param viewGroup passes in the viewGroup object
+     * @return
+     */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
         return LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1,
                 viewGroup, false);
     }
 
+    /**
+     * This function will help the view of the UI to be bound to the UI and therefore it
+     * will use the string for the note to be added to the notes.
+     * @param view passes the view object
+     * @param context passes the context object
+     * @param cursor passes in the cursor object
+     */
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
 
